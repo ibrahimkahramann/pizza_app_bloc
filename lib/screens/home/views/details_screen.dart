c1c0bc77aa9a0bbb973a04d95bc718099e82d8b7
ibrahimkahramann <ibrahimk.bg06@gmail.com>
2026-02-05 15:41:11 +0200

    Refactor DetailsScreen and HomeScreen to integrate pizza details and improve UI; add macro display and update navigation

--- lib/screens/home/views/details_screen.dart
+++ lib/screens/home/views/details_screen.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:pizza_repository/pizza_repository.dart';
+import '../../../components/macro.dart';
 
 class DetailsScreen extends StatelessWidget {
-  const DetailsScreen({super.key});
+  final Pizza pizza;
+  const DetailsScreen(this.pizza, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
+      backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.surface,
+        backgroundColor: Theme.of(context).colorScheme.background,
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         child: Column(
           children: [
             Container(
@@ -20,36 +24,35 @@ class DetailsScreen extends StatelessWidget {
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(30),
-                boxShadow: [
+                boxShadow: const [
                   BoxShadow(
-                    color: Colors.grey.shade300,
-                    blurRadius: 10,
-                    spreadRadius: 5,
-                    offset: const Offset(0, 5),
-                  ),
-                ],  
-                image: const DecorationImage(
-                  image: AssetImage('assets/1.png'),
-                  fit: BoxFit.cover,
-                ),
+                    color: Colors.grey,
+                    offset: Offset(3, 3),
+                    blurRadius: 5
+                  )
+                ],
+                image: DecorationImage(
+                  image: NetworkImage(
+                    pizza.picture
+                  )
+                )
               ),
             ),
-            SizedBox(height: 30),
+            const SizedBox(height: 30,),
             Container(
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(30),
-                boxShadow: [
+                boxShadow: const [
                   BoxShadow(
-                    color: Colors.grey.shade300,
-                    blurRadius: 10,
-                    spreadRadius: 5,
-                    offset: const Offset(0, 5),
-                  ),
-                ],  
+                    color: Colors.grey,
+                    offset: Offset(3, 3),
+                    blurRadius: 5
+                  )
+                ],
               ),
               child: Padding(
-                padding: const EdgeInsets.all(16.0),
+                padding: const EdgeInsets.all(20.0),
                 child: Column(
                   children: [
                     Row(
@@ -58,10 +61,10 @@ class DetailsScreen extends StatelessWidget {
                         Expanded(
                           flex: 2,
                           child: Text(
-                            "Truffle Temptation Extravaganza",
-                            style: TextStyle(
+                            pizza.name,
+                            style: const TextStyle(
                               fontSize: 20,
-                              fontWeight: FontWeight.w800
+                              fontWeight: FontWeight.bold
                             ),
                           ),
                         ),
@@ -73,20 +76,20 @@ class DetailsScreen extends StatelessWidget {
                               crossAxisAlignment: CrossAxisAlignment.end,
                               children: [
                                 Text(
-                                  "\$12.00",
+                                  "\$${pizza.price - (pizza.price * (pizza.discount) / 100)}",
                                   style: TextStyle(
                                     fontSize: 20,
-                                    fontWeight: FontWeight.w800,
-                                    color: Theme.of(context).colorScheme.primary,
+                                    fontWeight: FontWeight.bold,
+                                    color: Theme.of(context).colorScheme.primary
                                   ),
                                 ),
                                 Text(
-                                  "\$15.00",
-                                  style: TextStyle(
+                                  "\$${pizza.price}.00",
+                                  style: const TextStyle(
                                     fontSize: 16,
-                                    fontWeight: FontWeight.w800,
+                                    fontWeight: FontWeight.bold,
                                     color: Colors.grey,
-                                    decoration: TextDecoration.lineThrough,
+                                    decoration: TextDecoration.lineThrough
                                   ),
                                 ),
                               ],
@@ -94,15 +97,68 @@ class DetailsScreen extends StatelessWidget {
                           ),
                         )
                       ],
+                    ),
+                    const SizedBox(height: 12,),
+                    Row(
+                      children: [
+                        MyMacroWidget(
+                          title: "Calories",
+                          value: pizza.macros.calories,
+                          icon: FontAwesomeIcons.fire,
+                        ),
+                        const SizedBox(width: 10,),
+                        MyMacroWidget(
+                          title: "Protein",
+                          value: pizza.macros.protein,
+                          icon: FontAwesomeIcons.dumbbell,
+                        ),
+                        const SizedBox(width: 10,),
+                        MyMacroWidget(
+                          title: "Fat",
+                          value: pizza.macros.fats,
+                          icon: FontAwesomeIcons.oilWell,
+                        ),
+                        const SizedBox(width: 10,),
+                        MyMacroWidget(
+                          title: "Carbs",
+                          value: pizza.macros.carbs,
+                          icon: FontAwesomeIcons.breadSlice,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 40,),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      height: 50,
+                      child: TextButton(
+                        onPressed: () {
+                          
+                        },
+                        style: TextButton.styleFrom(
+                          elevation: 3.0,
+                          backgroundColor: Colors.black,
+                          foregroundColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)
+                          )
+                        ),
+                        child: const Text(
+                          "Buy Now",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600
+                          ),
+                        ),
+                      ),
                     )
                   ],
                 ),
               ),
             )
-
           ],
         ),
-      )
+      ),
     );
   }
 }
--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -2,9 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pizza_app/screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
+import 'package:pizza_app/screens/home/blocs/get_pizza_bloc/get_pizza_bloc.dart';
 import 'package:pizza_app/screens/home/views/details_screen.dart';
-
-import '../blocs/get_pizza_bloc/get_pizza_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -12,177 +11,164 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.surface,
+      backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.surface,
+        backgroundColor: Theme.of(context).colorScheme.background,
         title: Row(
           children: [
-            Image.asset('assets/8.png', height: 30),
+            Image.asset('assets/8.png', scale: 14),
             const SizedBox(width: 8),
-            Text(
-              "PIZZA",
+            const Text(
+              'PIZZA',
               style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30),
-            ),
+            )
           ],
         ),
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.cart)),
+          IconButton(onPressed: () {}, icon: const Icon(CupertinoIcons.cart)),
           IconButton(
-            onPressed: () {
-              context.read<SignInBloc>().add(SignOutRequired());
-            },
-            icon: Icon(CupertinoIcons.arrow_right_to_line),
-          ),
+              onPressed: () {
+                context.read<SignInBloc>().add(SignOutRequired());
+              },
+              icon: const Icon(CupertinoIcons.arrow_right_to_line)),
         ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: BlocBuilder<GetPizzaBloc, GetPizzaState>(
           builder: (context, state) {
-            if(state is GetPizzaSuccess){
-            return GridView.builder(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-                childAspectRatio: 9 / 16,
-              ),
-              itemCount: state.pizzas.length,
-              itemBuilder: (context, int i) {
-                return Material(
-                  elevation: 3,
-                  color: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: InkWell(
-                    borderRadius: BorderRadius.circular(20),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const DetailsScreen(),
-                        ),
-                      );
-                    },
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Image.network(state.pizzas[i].picture,
-                            height: 150,
-                            width: double.infinity,
-                            fit: BoxFit.cover),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                          child: Row(
-                            children: [
-                              Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.red,
-                                  borderRadius: BorderRadius.circular(30),
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    horizontal: 8.0,
-                                    vertical: 4.0,
-                                  ),
-                                  child: Text(
-                                    'NON-VEG',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 10,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              SizedBox(width: 8),
-                              Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.green.withOpacity(0.1),
-                                  borderRadius: BorderRadius.circular(30),
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    horizontal: 8.0,
-                                    vertical: 4.0,
-                                  ),
-                                  child: Text(
-                                    '🌶️BALANCE',
-                                    style: TextStyle(
-                                      color: Colors.green,
-                                      fontWeight: FontWeight.w800,
-                                      fontSize: 10,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                          child: Text(
-                            "Cheesy Marvel",
-                            style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                          child: Text(
-                            "Crafting joy: your pizza, your rules, best taste!",
-                            style: TextStyle(
-                              fontSize: 10,
-                              color: Colors.grey.shade700,
-                            ),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                          child: Row(
-                            children: [
-                              Text(
-                                "\$12.00",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  color: Theme.of(context).colorScheme.primary,
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              ),
-                              const SizedBox(width: 5),
-                              Text(
-                                "\$15.00",
-                                style: TextStyle(
-                                  fontSize: 12,
-                                  color: Colors.grey.shade500,
-                                  fontWeight: FontWeight.w700,
-                                  decoration: TextDecoration.lineThrough,
-                                ),
-                              ),
-                              const Spacer(),
-                              IconButton(
-                                onPressed: () {},
-                                icon: const Icon(
-                                  CupertinoIcons.add_circled_solid,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
+            if(state is GetPizzaSuccess) {
+              return GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, crossAxisSpacing: 16, mainAxisSpacing: 16, childAspectRatio: 9 / 16),
+                itemCount: state.pizzas.length,
+                itemBuilder: (context, int i) {
+                  return Material(
+                    elevation: 3,
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
                     ),
-                  ),
-                );
-              },
-            );
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(20),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute<void>(
+                            builder: (BuildContext context) => DetailsScreen(
+                              state.pizzas[i]
+                            ),
+                          ),
+                        );
+                      },
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.network(state.pizzas[i].picture),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                            child: Row(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    color: state.pizzas[i].isVeg
+                                      ? Colors.green
+                                      : Colors.red,
+                                    borderRadius: BorderRadius.circular(30)
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                                    child: Text(
+                                      state.pizzas[i].isVeg
+                                        ? "VEG"
+                                        : "NON-VEG",
+                                      style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 10),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(width: 8),
+                                Container(
+                                  decoration: BoxDecoration(color: Colors.green.withOpacity(0.2), borderRadius: BorderRadius.circular(30)),
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+                                    child: Text(
+                                      state.pizzas[i].spicy == 1
+                                        ? "🌶️ BLAND"
+                                        : state.pizzas[i].spicy == 2
+                                          ? "🌶️ BALANCE"
+                                          : "🌶️ SPICY",
+                                      style: TextStyle(
+                                        color: state.pizzas[i].spicy == 1
+                                        ? Colors.green
+                                        : state.pizzas[i].spicy == 2
+                                          ? Colors.orange
+                                          : Colors.redAccent, 
+                                        fontWeight: FontWeight.w800, 
+                                        fontSize: 10
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                            child: Text(
+                              state.pizzas[i].name,
+                              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                            child: Text(
+                               state.pizzas[i].description,
+                              style: TextStyle(
+                                fontSize: 10,
+                                color: Colors.grey.shade500,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Text(
+                                      "\$${state.pizzas[i].price - (state.pizzas[i].price * (state.pizzas[i].discount) / 100)}",
+                                      style: TextStyle(fontSize: 16, color: Theme.of(context).colorScheme.primary, fontWeight: FontWeight.w700),
+                                    ),
+                                    const SizedBox(
+                                      width: 5,
+                                    ),
+                                    Text(
+                                      "\$${state.pizzas[i].price}.00",
+                                      style: TextStyle(fontSize: 12, color: Colors.grey.shade500, fontWeight: FontWeight.w700, decoration: TextDecoration.lineThrough),
+                                    ),
+                                  ],
+                                ),
+                                IconButton(onPressed: () {}, icon: const Icon(CupertinoIcons.add_circled_solid))
+                              ],
+                            )
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                }
+              );
             } else if(state is GetPizzaLoading) {
-              return Center(child: CircularProgressIndicator());
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
             } else {
-              return Center(child: Text('Something went wrong!'));
+              return const Center(
+                child: Text(
+                  "An error has occured..."
+                ),
+              );
             }
           },
         ),
